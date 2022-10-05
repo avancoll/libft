1c936a9a58975ddf14a7eb451bd5ee5573297ff0
Antoine Vancollie <113617810+avancoll@users.noreply.github.com>
2022-10-05 21:27:15 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -41,3 +41,31 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[i + j] = '\0';
 	return (tot);
 }
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] && i < dstsize)
+		i++;
+	if (dstsize > i)
+	{
+		while (src[j] && j < dstsize - i - 1)
+		{
+			dst[i + j] = src[j];
+			j++;
+		}
+		dst[i + j] = 0;
+	}
+	j = 0;
+	while (src[j])
+		j++;
+	if (dstsize < i)
+		return (dstsize + j);
+	return (i + j);
+}

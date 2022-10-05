19f3b99a4c93dc5f42012ce7f566f1bdf78957f6
Antoine Vancollie <113617810+avancoll@users.noreply.github.com>
2022-10-05 21:28:14 +0200

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -30,3 +30,22 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	}
 	return (i);
 }
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	while (src[j])
+		j++;
+	if (dstsize == 0)
+		return (j);
+	i = -1;
+	while (src[++i] && i < dstsize - 1)
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (j);
+}

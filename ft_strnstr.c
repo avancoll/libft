57b5bf97898a47850c4b5411e8b930be42a943b9
Antoine Vancollie <113617810+avancoll@users.noreply.github.com>
2022-10-05 21:48:29 +0200

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -38,3 +38,26 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (0);
 }
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!*needle)
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			while (needle[j] && needle[j] == haystack[i + j] && (i + j) < len)
+				j++;
+			if (!needle[j])
+				return ((char *)haystack + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
